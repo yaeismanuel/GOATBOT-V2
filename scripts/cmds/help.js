86e0e1cc7d6ac5428a99b445546ac34b75b2e105
yaeismanuel <rickyportu@gmail.com>
2025-02-13 22:59:59 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🦋 ArYAN 🌺";
+const doNotDelete = "THOMAS BOT";
 /** 
 * @author NTKhang
 * @author: do not delete it
